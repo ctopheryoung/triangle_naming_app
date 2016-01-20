3a09630a4396aa7cc122a45aaab1a7fa6fa861af
Chris Young and Trevor Elvey <trevor.elvey@gmail.com>
2016-01-19 16:52:20 -0800

    wrote user interface script

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,32 @@ var triangleCheck = function (lengthA,lengthB,lengthC) {
     return "notTriangle";
   } else if (lengthA === lengthB && lengthB === lengthC) {
     return "equilateralTriangle";
-  } else if (lengthA === lengthB && lengthA !== lengthC || lengthA === lengthC && lengthA !==lengthB) {
+  } else if (lengthA === lengthB && lengthA !== lengthC || lengthA === lengthC && lengthA !==lengthB || lengthB === lengthC && lengthB !== lengthA) {
     return "isoscelesTriangle";
   } else if (lengthA !== lengthB && lengthB !== lengthC) {
     return "scaleneTriangle";
   }
 };
+
+$(document).ready(function() {
+  $("form#dimensions").submit(function(event) {
+    var lengthA = parseInt($("input#lengthA").val());
+    var lengthB = parseInt($("input#lengthB").val());
+    var lengthC = parseInt($("input#lengthC").val());
+    var result = triangleCheck (lengthA,lengthB,lengthC)
+
+    if (result === "notTriangle") {
+      $(".triangleType").text("not a real triangle, idiot");
+    } else if (result === "equilateralTriangle") {
+      $(".triangleType").text("an equilateral triangle");
+    } else if (result === "isoscelesTriangle") {
+      $(".triangleType").text("an isosceles triangle");
+    } else if (result === "scaleneTriangle") {
+      $(".triangleType").text("an scalene triangle");
+    }
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
